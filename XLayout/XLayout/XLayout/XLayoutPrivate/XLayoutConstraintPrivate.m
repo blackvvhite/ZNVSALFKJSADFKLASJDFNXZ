975a87df8063ffde210909371abdf94543ddc5e9
leijingjiang <leijinjiang@yuanpinda.com>
2016-05-04 17:50:24 +0800

    修改宽高只给倍数时无法获取父视图导致不生效的bug

--- XLayout/XLayout/XLayout/XLayoutPrivate/XLayoutConstraintPrivate.m
+++ XLayout/XLayout/XLayout/XLayoutPrivate/XLayoutConstraintPrivate.m
@@ -35,6 +35,9 @@
         self.secondAttribute = self.firstAttribute;
     }else if (self.secondAttribute != NSLayoutAttributeNotAnAttribute && !self.secondView) {
         self.secondView = self.firstView.superview;
+    }else if (self.secondAttribute == NSLayoutAttributeNotAnAttribute && !self.secondView && self.multiplier != 1.0) {
+        self.secondAttribute = self.firstAttribute;
+        self.secondView = self.firstView.superview;
     }
     
     NSPredicate *pre = [NSPredicate predicateWithFormat:@"firstItem == %@ and firstAttribute == %d and relation == %d and secondItem == %@ and secondAttribute == %d and multiplier == %f",self.firstView,self.firstAttribute,self.relation,self.secondView,self.secondAttribute,self.multiplier];
